26599d6dbe9430e2bed642a8718cac97f4ef8ebc
Nakul Padalkar <nakulpadalkar@gmail.com>
2018-05-21 18:32:37 -0500

    Update firstproject.py

--- firstproject.py
+++ firstproject.py
@@ -1,1 +1,17 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import requests
+import json
+import hmac
+import hashlib
+import base64
+import csv
+import io
+import logging
+from openpyxl import load_workbook
+from pandas.io.json import json_normalize
+
+for i in range(0,10):
+  print("This is trial "+i)
 
